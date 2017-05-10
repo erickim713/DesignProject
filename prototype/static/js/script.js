caab076f9272a74c3cda38ee2ae9e6d2196a78c8
kyungyunlee <kyungyunlee2393@gmail.com>
2017-05-10 23:59:48 +0900

    added prev, next page, image popup modal

--- prototype/static/js/script.js
+++ prototype/static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.btnBack').click(function() {
+	$('.btnBack').click(function() {s
 		window.history.back();
 	});
 });
@@ -10,19 +10,22 @@ $(document).ready(function() {
 // Get the modal
 var modal = document.getElementById("ind_history_img_modal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("ind_history_img1");
-var modalImg = document.getElementById("drinking1");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+var allImages = document.getElementsByClassName('ind_history_img');
+for(var i = 0; i < allImages.length; i++){
+	var img = allImages[i];
+	var modalImg = document.getElementById("drinking");
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		console.log(this.src);
+	}
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
+  modal.style.display = "none";
+}
+modal.onclick = function() {
   modal.style.display = "none";
 }
