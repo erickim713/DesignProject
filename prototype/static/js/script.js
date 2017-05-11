ab6a8f52fef636ffc908bf0d46b1a44365950199
Sangyeob Lee <sy0414@gmail.com>
2017-05-11 09:42:21 +0900

    hamburger menu added

--- prototype/static/js/script.js
+++ prototype/static/js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 	$('.btnBack').click(function() {
 		window.history.back();
 	});
+	$('#top_nav .right').click(function() {
+		$('#menu_nav').toggleClass('opened');
+	});
+	$('#menu_nav .background').click(function() {
+		$('#menu_nav').toggleClass('opened');
+	});
 });
