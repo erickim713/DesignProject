495ee0904d44382cb5811faec774bc333925ad34
Sangyeob Lee <sy0414@gmail.com>
2017-05-29 04:24:45 +0900

    fix view

--- prototype/static/js/script.js
+++ prototype/static/js/script.js
@@ -11,3 +11,11 @@ $(document).ready(function() {
     $('.ui-datepicker-current-day').click(function(){console.log($('#datepicker').val());
     });
 });
+
+$('div#image_modal div.background').click(function() {
+  $('div#image_modal').addClass('disp-none');
+}); 
+$('#photo_grid div.imgwrapper').click(function() {
+  $('div#image_modal').removeClass('disp-none');
+  $('div#image_modal img').attr('src', $(this).attr('data-img-src'));
+});
