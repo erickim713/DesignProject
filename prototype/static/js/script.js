f7a556c3ca3cb53ed5c32c6a981155ae34becd87
kyungyunlee <kyungyunlee2393@gmail.com>
2017-05-28 23:49:30 +0900

    change design

--- prototype/static/js/script.js
+++ prototype/static/js/script.js
@@ -11,3 +11,28 @@ $(document).ready(function() {
     $('.ui-datepicker-current-day').click(function(){console.log($('#datepicker').val());
     });
 });
+
+// history page
+// image modal
+// Get the modal
+var modal = document.getElementById("ind_history_img_modal");
+
+var allImages = document.getElementsByClassName('ind_history_img');
+for(var i = 0; i < allImages.length; i++){
+	var img = allImages[i];
+	var modalImg = document.getElementById("drinking");
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		console.log(this.src);
+	}
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+modal.onclick = function() {
+  modal.style.display = "none";
+}
